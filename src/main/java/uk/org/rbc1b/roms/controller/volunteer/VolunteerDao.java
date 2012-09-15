84e33e87f40b2434eee3b40ae16f790336539c06
rahulsingh <rahulsingh@ubws01>
2012-09-15 13:23:28 +0100

    Added VolunterDAO as a test to see commit process.

--- src/main/java/uk/org/rbc1b/roms/controller/volunteer/VolunteerDao.java
+++ src/main/java/uk/org/rbc1b/roms/controller/volunteer/VolunteerDao.java
@@ -4,22 +4,10 @@
  */
 package uk.org.rbc1b.roms.controller.volunteer;
 
-
-import java.util.List;
-import uk.org.rbc1b.roms.db.Volunteer;
 /**
  *
  * @author rahulsingh
  */
 public interface VolunteerDao {
     
-    /*
-     * Find a Volunteer by the RBCID number
-     */
-    Volunteer findVolunteer(long rbcid);
-    
-    /*
-     * Find all Volunteers in the database.
-     */
-    List<Volunteer> findVolunteers();
 }
