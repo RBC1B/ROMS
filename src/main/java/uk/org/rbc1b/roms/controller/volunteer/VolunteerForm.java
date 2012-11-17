b99f2227534598464f02496d84c0b559f8f22b5a
Oliver Elder <oliver@wahanda.com>
2012-11-17 16:10:59 +0000

    make a start on the volunteer edit form. The layout is based on the paper application form

--- src/main/java/uk/org/rbc1b/roms/controller/volunteer/VolunteerForm.java
+++ src/main/java/uk/org/rbc1b/roms/controller/volunteer/VolunteerForm.java
@@ -4,6 +4,7 @@
  */
 package uk.org.rbc1b.roms.controller.volunteer;
 
+import java.sql.Date;
 import uk.org.rbc1b.roms.db.Person;
 
 /**
@@ -13,7 +14,62 @@ import uk.org.rbc1b.roms.db.Person;
  */
 public class VolunteerForm {
 
+    private Date baptismDate;
+    private Integer congregationId;
+    private boolean elder;
+    private Integer emergencyRelationshipId;
+    private String gender;    // M or F
+    private boolean ministerialServant;
     private Person person = new Person();
+    private boolean regularPioneer;
+
+    public Date getBaptismDate() {
+        return baptismDate;
+    }
+
+    public void setBaptismDate(Date baptismDate) {
+        this.baptismDate = baptismDate;
+    }
+
+    public Integer getCongregationId() {
+        return congregationId;
+    }
+
+    public void setCongregationId(Integer congregationId) {
+        this.congregationId = congregationId;
+    }
+
+    public boolean isElder() {
+        return elder;
+    }
+
+    public void setElder(boolean elder) {
+        this.elder = elder;
+    }
+
+    public Integer getEmergencyRelationshipId() {
+        return emergencyRelationshipId;
+    }
+
+    public void setEmergencyRelationshipId(Integer emergencyRelationshipId) {
+        this.emergencyRelationshipId = emergencyRelationshipId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isMinisterialServant() {
+        return ministerialServant;
+    }
+
+    public void setMinisterialServant(boolean ministerialServant) {
+        this.ministerialServant = ministerialServant;
+    }
 
     public Person getPerson() {
         return person;
@@ -21,5 +77,13 @@ public class VolunteerForm {
 
     public void setPerson(Person person) {
         this.person = person;
+    }
+
+    public boolean isRegularPioneer() {
+        return regularPioneer;
+    }
+
+    public void setRegularPioneer(boolean regularPioneer) {
+        this.regularPioneer = regularPioneer;
     }
 }
