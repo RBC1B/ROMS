70f7f2a5f4fa9a25f5428854d8a926976ab09a4c
dongagon <nigelnroberts@gmail.com>
2013-01-06 21:50:15 +0000

    Update database/008.sql

--- database/008.sql
+++
@@ -1,3 +0,0 @@
-*/ Audit table creation script */
-
-CREATE  TABLE `roms`.`Audit` (  `AuditID` BIGINT NOT NULL ,  `Field` VARCHAR(60) NULL ,  `OldValue` VARCHAR(60) NULL ,  `NewValue` VARCHAR(60) NULL ,  PRIMARY KEY (`AuditID`) );
