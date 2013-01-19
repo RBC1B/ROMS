d436476843d5832bc423bb5c834e5b1176fd2313
oliver elder <oliver.elder.esq@gmail.com>
2013-01-19 16:14:48 +0000

    Split the dummy data out from the schema changes

---
+++ database/002-dummy.sql
@@ -0,0 +1,11 @@
+/* Add the CircuitOverseerId to the Circuit table */
+use ROMS;
+
+update Circuit set CircuitOverseerId = 1
+where CircuitId = 1;
+
+update Circuit set CircuitOverseerId = 2
+where CircuitId = 2;
+
+update Circuit set CircuitOverseerId = 4
+where CircuitId = 3;
--- database/002.sql
+++ database/002.sql
@@ -1,25 +1,6 @@
-/*Script to Alter the Circuit table */
+/* Add the CircuitOverseerId to the Circuit table */
+use ROMS;
 
-
-ALTER TABLE Circuit ADD COLUMN CircuitOverseerID BIGINT(20);
-
-/*Script to add FK relationship to Person table */
-
-
-ALTER TABLE Circuit ADD 
-      CONSTRAINT FK_Person
-      FOREIGN    KEY (CircuitOverseerID)
-      REFERENCES Person(PersonID);
-
-
-/*Script to add values to CircuitOverseerId column */
-
-
-update ROMS.Circuit set CircuitOverseerId = 1
-where CircuitID = 1;
-
-update ROMS.Circuit set CircuitOverseerId = 2
-where CircuitID = 2;
-
-update ROMS.Circuit set CircuitOverseerId = 4
-where CircuitID = 3;
+alter table Circuit
+    add column CircuitOverseerId bigint(20),
+    add constraint foreign key (CircuitOverseerId) references Person(PersonId) on delete cascade;
