38399dcb519429b66c43b48ebfc5817a12e85e14
rahulsingh <rahulsingh@ubws01>
2013-01-26 15:22:47 +0000

    Updated CircuitForm for editing function in Circuits.

--- src/main/java/uk/org/rbc1b/roms/controller/circuit/CircuitForm.java
+++ src/main/java/uk/org/rbc1b/roms/controller/circuit/CircuitForm.java
@@ -6,6 +6,7 @@ package uk.org.rbc1b.roms.controller.circuit;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import uk.org.rbc1b.roms.db.Person;
 
 /**
  * Request form bean when creating/editing the circuit.
@@ -17,6 +18,7 @@ public class CircuitForm {
     @NotNull
     @Size(max = 50)
     private String name;
+    private Person person = new Person();
 
     /**
      * @return name
@@ -30,5 +32,19 @@ public class CircuitForm {
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * @return the person
+     */
+    public Person getPerson() {
+        return person;
+    }
+
+    /**
+     * @param person the person to set
+     */
+    public void setPerson(Person person) {
+        this.person = person;
     }
 }
