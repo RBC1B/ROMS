507e6ae5b6d5a45c2fe5f575a5c09e2f7f288e04
oliver elder <oliver.elder.esq@gmail.com>
2013-06-08 00:57:47 +0100

    [#50953477] reinstate unit test after permissions evaluator reworking

--- src/test/java/uk/org/rbc1b/roms/security/RomsPermissionEvaluatorTest.java
+++ src/test/java/uk/org/rbc1b/roms/security/RomsPermissionEvaluatorTest.java
@@ -4,13 +4,11 @@
  */
 package uk.org.rbc1b.roms.security;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -28,28 +26,27 @@ public class RomsPermissionEvaluatorTest {
     @Before
     public void setUp() {
         evaluator = new RomsPermissionEvaluator();
-        authentication = new MockAuthentication(Arrays.asList("AuthA:4", "AuthB:2", "AuthC:3"));
+        authentication = new MockAuthentication(1, "testname", "passwd");
+        authentication.addAuthority("AuthA", "ApplicationA", 4, 5);
+        authentication.addAuthority("AuthB", "ApplicationB", 2, 5);
+        authentication.addAuthority("AuthC", "Applicationc", 3, 5);
     }
 
-    @Ignore
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidPermission() {
         evaluator.hasPermission(authentication, "AuthC", "Invalid");
     }
 
-    @Ignore
     @Test
     public void testValidPermissionTargetNotFound() {
         assertEquals(false, evaluator.hasPermission(authentication, "AuthUnknown", "READ"));
     }
 
-    @Ignore
     @Test
     public void testValidPermissionDenied() {
         assertEquals(false, evaluator.hasPermission(authentication, "AuthB", "ADD"));
     }
 
-    @Ignore
     @Test
     public void testValidPermissionAllowed() {
         assertEquals(true, evaluator.hasPermission(authentication, "AuthC", "ADD"));
@@ -58,18 +55,29 @@ public class RomsPermissionEvaluatorTest {
 
     private class MockAuthentication implements Authentication {
 
-        List<MockGrantedAuthority> authorities;
+        private Map<String, ROMSGrantedAuthority> authorities = new HashMap<String, ROMSGrantedAuthority>();
+        private Integer userId;
+        private String password;
+        private String userName;
 
-        private MockAuthentication(Collection<String> authorities) {
-            this.authorities = new ArrayList<MockGrantedAuthority>(authorities.size());
-            for (String authority : authorities) {
-                this.authorities.add(new MockGrantedAuthority(authority));
-            }
+        private MockAuthentication(Integer userId, String userName, String password) {
+            this.userId = userId;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        private void addAuthority(String code, String application, Integer departmentLevel, Integer nonDepartmentLevel) {
+            ROMSGrantedAuthority authority = new ROMSGrantedAuthority();
+            authority.setApplication(application);
+            authority.setDepartmentLevelAccess(departmentLevel);
+            authority.setNonDepartmentLevelAccess(nonDepartmentLevel);
+
+            authorities.put(code, authority);
         }
 
         @Override
         public Collection<? extends GrantedAuthority> getAuthorities() {
-            return this.authorities;
+            return this.authorities.values();
         }
 
         @Override
@@ -84,22 +92,67 @@ public class RomsPermissionEvaluatorTest {
 
         @Override
         public Object getPrincipal() {
-            throw new UnsupportedOperationException("Not supported yet.");
+            return new ROMSUserDetails() {
+                @Override
+                public ROMSGrantedAuthority findAuthority(String application) {
+                    return authorities.get(application);
+                }
+
+                @Override
+                public Integer getUserId() {
+                    return userId;
+                }
+
+                @Override
+                public Collection<? extends GrantedAuthority> getAuthorities() {
+                    throw new UnsupportedOperationException("Not supported yet.");
+                }
+
+                @Override
+                public String getPassword() {
+                    return password;
+                }
+
+                @Override
+                public String getUsername() {
+                    return userName;
+                }
+
+                @Override
+                public boolean isAccountNonExpired() {
+                    return true;
+                }
+
+                @Override
+                public boolean isAccountNonLocked() {
+                    return true;
+                }
+
+                @Override
+                public boolean isCredentialsNonExpired() {
+                    return true;
+                }
+
+                @Override
+                public boolean isEnabled() {
+                    return true;
+                }
+            };
         }
 
         @Override
         public boolean isAuthenticated() {
-            throw new UnsupportedOperationException("Not supported yet.");
+            return true;
         }
 
         @Override
         public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-            throw new UnsupportedOperationException("Not supported yet.");
+            // do nothing
         }
 
         @Override
         public String getName() {
-            throw new UnsupportedOperationException("Not supported yet.");
+            return userName;
         }
     }
 
