5425d55dfae197071428efc994acb192953ba8d7
rs146 <rs146.london@gmail.com>
2013-10-13 21:33:31 +0100

    [#50560123] Created the class for the Edit/Create KH form.

--- src/main/java/uk/org/rbc1b/roms/controller/kingdomhall/KingdomHallForm.java
+++ src/main/java/uk/org/rbc1b/roms/controller/kingdomhall/KingdomHallForm.java
@@ -23,10 +23,102 @@
  */
 package uk.org.rbc1b.roms.controller.kingdomhall;
 
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  * Kingdom Hall form bean for creating/editing a kingdom hall.
  *
  * @author rahulsingh
  */
 public class KingdomHallForm {
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    private String name;
+    @Size(min = 2, max = 50)
+    private String steet;
+    @Size(min = 2, max = 50)
+    private String town;
+    @Size(min = 2, max = 50)
+    private String county;
+    @Size(min = 4, max = 7)
+    private String postcode;
+    private Integer owningCongregationId;
+    private String owningCongregationName;
+    private Integer ownershipTypeId;
+    private List<String> congregations;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSteet() {
+        return steet;
+    }
+
+    public void setSteet(String steet) {
+        this.steet = steet;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public Integer getOwningCongregationId() {
+        return owningCongregationId;
+    }
+
+    public void setOwningCongregationId(Integer owningCongregationId) {
+        this.owningCongregationId = owningCongregationId;
+    }
+
+    public String getOwningCongregationName() {
+        return owningCongregationName;
+    }
+
+    public void setOwningCongregationName(String owningCongregationName) {
+        this.owningCongregationName = owningCongregationName;
+    }
+
+    public Integer getOwnershipTypeId() {
+        return ownershipTypeId;
+    }
+
+    public void setOwnershipTypeId(Integer ownershipTypeId) {
+        this.ownershipTypeId = ownershipTypeId;
+    }
+
+    public List<String> getCongregations() {
+        return congregations;
+    }
+
+    public void setCongregations(List<String> congregations) {
+        this.congregations = congregations;
+    }
 }
