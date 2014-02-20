8dfe120d84ab106310847069f51e14891ac855ff
oliver elder <oliver.elder.esq@gmail.com>
2014-02-20 01:00:36 +0000

    [#64976922] add a missing semi-colon to the last statement

--- database/003-dummy.sql
+++ database/003-dummy.sql
@@ -10,4 +10,4 @@ insert into VolunteerInterviewSession (InterviewSessionId, PersonId, VolunteerIn
 values 
     (1, 6, 'IT', null, '2014-02-13 18:27:00', 0),
     (1, 8, 'CC', 'Washing his hair that day', '2014-02-13 18:27:00', 0),
-    (2, 9, 'CP', 'Was not wearing socks!', '2014-02-13 18:27:00', 0)    
+    (2, 9, 'CP', 'Was not wearing socks!', '2014-02-13 18:27:00', 0);
