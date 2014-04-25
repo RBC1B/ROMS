4d7a0ce6853b3334782c7268fcbe67f46eed02ed
oliver elder <oliver.elder.esq@gmail.com>
2014-04-25 17:22:57 +0100

    [#51348755] Correct the field name on the person form work phone validation

--- src/main/webapp/javascript/persons.js
+++ src/main/webapp/javascript/persons.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
                 phoneNumber: true,
                 mobilePhoneNumber: true
             },
-            workphone: {
+            workPhone: {
                 phoneNumber: true
             }
         },
