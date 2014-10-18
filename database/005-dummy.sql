076449a67a27653352f93dcf6f238300ab89d391
oliver elder <oliver.elder.esq@gmail.com>
2014-10-18 11:42:47 +0100

    [#80070420] Syntax fix

--- database/005-dummy.sql
+++ database/005-dummy.sql
@@ -1,3 +1,4 @@
 insert into Project(Name, KingdomHallId, CoordinatorId, MinorWork, RequestDate) values
 ('KH 1', 1, 1, true, '2014-09-23'),
-('KH 2', 2, 2, false, '2014-01-02';
+('KH 2', 2, 2, false, '2014-01-02');
+
